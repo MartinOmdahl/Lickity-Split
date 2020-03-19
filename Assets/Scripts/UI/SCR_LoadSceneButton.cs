c97f1cdf24131275e0b671eca1cc764dac8976b0
Martin B Omdahl <martinbrom@hotmail.no>
2020-03-19 13:22:16 +0100

    Made Start and Credits menus functional
    Loading scenes and quitting game now works properly.
    Made Start Menu the first scene in Build Settings.

---
+++ Assets/Scripts/UI/SCR_LoadSceneButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Script is for simple UI interaction that should just load a scene.
+public class SCR_LoadSceneButton : MonoBehaviour
+{
+    #region Public variables
+    public enum SceneLoadType { Name, Index};
+    [Tooltip("Should scene index or scene name be used to load scene?")]
+    public SceneLoadType loadSceneBy = SceneLoadType.Name;
+
+    [Tooltip("Name of scene to load (only used if loadSceneBy is set to \"Name\")")]
+    public string sceneName;
+    [Tooltip("Index of scene to load (only used if loadSceneBy is set to \"Index\")")]
+    public int sceneIndex;
+    #endregion
+
+    /// <summary>
+    /// Load the specified scene.
+    /// Function is called by button on UI canvas.
+    /// </summary>
+    public void LoadScene()
+    {
+        switch (loadSceneBy)
+        {
+            case SceneLoadType.Name:
+                if (sceneName != "")
+                    SceneManager.LoadScene(sceneName);
+                else
+                    print("Error: scene name not set");
+                break;
+            case SceneLoadType.Index:
+                SceneManager.LoadScene(sceneIndex);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/SCR_StartMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class SCR_StartMenu : MonoBehaviour
+{
+    public Transform buttonHighlighter;
+
+    EventSystem eventSystem;
+
+
+    private void Start()
+    {
+        eventSystem = EventSystem.current;
+    }
+
+    private void Update()
+    {
+        HighlightSelectedButton();
+    }
+
+    void HighlightSelectedButton()
+    {
+        if (eventSystem.currentSelectedGameObject != null)
+            buttonHighlighter.position = Vector3.Lerp(buttonHighlighter.position, eventSystem.currentSelectedGameObject.transform.position, 50 * Time.deltaTime);
+    }
+
+    public void StartGameButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene("SCE_Credits");
+    }
+
+    public void QuitGameButton()
+    {
+        Application.Quit();
+    }
+
+
+}
