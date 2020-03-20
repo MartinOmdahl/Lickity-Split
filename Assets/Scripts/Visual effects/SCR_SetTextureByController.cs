e9048ec53bfe8787fa3d9d1921c85d2db76b3acd
Martin B Omdahl <martinbrom@hotmail.no>
2020-03-20 18:05:23 +0100

    Added swing tutorial, level tweaks, visual polish

---
+++ Assets/Scripts/Visual effects/SCR_SetTextureByController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SCR_SetTextureByController : MonoBehaviour
+{
+    // Update to include Switch in future
+    public Texture xboxTexture, playStationTexture;
+
+    MeshRenderer meshRenderer;
+    Gamepad gamepad;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Start()
+    {
+        gamepad = Gamepad.current;
+
+        if (gamepad != null)
+        {
+            string gamepadType = gamepad.GetType().BaseType.ToString();
+            if (gamepadType == "UnityEngine.InputSystem.DualShock.DualShockGamepad")
+            {
+                // If controller is PlayStation, set texture to PlayStation version
+                meshRenderer.material.mainTexture = playStationTexture;
+            }
+            else
+            {
+                // Xbox version is default
+                meshRenderer.material.mainTexture = xboxTexture;
+            }
+        }
+    }
+}
